c7e715253b92dcd0d57dde1f5a6d9f493ed23a8c
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-11 15:40:19 +0100

    add messagge and image

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ const app = new Vue({
   el: "#app",
   data: {
     message: "Hello Vue!",
+    classMessagge: "main-title",
+    image: "https://placekitten.com/g/300/200",
+    title: "gattino adorabile",
   },
 });
